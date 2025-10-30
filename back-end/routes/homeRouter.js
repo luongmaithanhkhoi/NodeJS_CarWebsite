6a35fb1557a19285e428c76dc68064d0b5349df3
Giahao <Admin@LAPTOP-1JGLQK4M>
2025-10-31 00:13:35 +0700

    gia hao

--- back-end/routes/homeRouter.js
+++ back-end/routes/homeRouter.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const {getAllCar, getCarById} = require('../controllers/homeController');
-const { getCarFilter } = require('../controllers/homeController');
+const { getCarFilter, updateCar, deleteCar } = require('../controllers/homeController');
 const router = express.Router();
 
 router.get('/allcars', getAllCar);
 router.post('/carinf/:id', getCarById);
 router.get('/cars', getCarFilter);
-// router.delete('/cars/:id', deleteCar);  
-// router.put('/cars/:id', updateCar);     
+router.delete('/cars/:id', deleteCar);  
+router.put('/cars/:id', updateCar);     
 
 module.exports = router;
